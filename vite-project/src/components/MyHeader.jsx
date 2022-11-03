35ca83e881a4ea3194bbf67dd3ba921492c467e0
Panni Belevski <pannibel@gmail.com>
2022-11-03 13:55:53 +0100

    new folder structure

--- vite-project/src/components/MyHeader.jsx
+++ vite-project/src/components/MyHeader.jsx
@@ -3,9 +3,10 @@ export default function MyHeader() {
 
     return (
         <header>
-            <h2 className="logo">Byont</h2>
-                <NavList />
-            <button>Contact us</button>
+                <h2 className="logo">Byont</h2>
+                    <NavList />
+                <button>Contact us</button>
+
         </header>
     )
 }
